7c7d5d9d7d044cdf3ab6c2523f45da5fc70ce42f
dilanflores <85522202+DilanQuimbia@users.noreply.github.com>
2021-08-03 21:13:30 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int arreglo1[5]={3,2,1,5,4};
+  int i,j,aux,min;
+  for(i=0;i<5;i++)
+  {
+    min = i;
+    for(j=i+1 ; j<5 ; j++)
+    {
+      if(arreglo1[j]<arreglo1[min])
+      {
+        min=j;
+      }
+    }
+    aux=arreglo1[i];
+    arreglo1[i]=arreglo1[min];
+    arreglo1[min]=aux;
+  }
+  for(int i=0; i <5 ; i++)
+  {
+    cout<< arreglo1[i];
+  }
+  return 0;
+}
